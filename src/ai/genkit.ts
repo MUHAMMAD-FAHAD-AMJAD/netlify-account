0b822e4599d72e0f1f2e35411f38c9102cc54e58
Muhammad Fahad Amjad <muhammadfahadamjad27@gmail.com>
2025-07-31 13:24:45 +0000

    Update API key and other files

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
-  model: 'googleai/gemini-2.0-flash',
+  plugins: [googleAI({apiKey: "AIzaSyCtki8H33DPpTger2DCYuNFftvdhmcpHnU"})],
+  model: 'googleai/gemini-2.5-pro',
 });
